ecba7c49c90d2245c5c072d49343da9a141c19b7
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-13 15:36:30 +0300

    feat: AppUser sınıfı oluşturuldu.
    Üyelik sistemi için Identity kütüphanesinden faydalanacağız. Dolayısıyla AppUser sınıfının yanında gerekli Identity paketleri class library için kuruldu.

---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.Entities/AppUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onicorn.CRMApp.Entities
+{
+    public class AppUser : IdentityUser<int>
+    {
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? ImageURL { get; set; }
+
+        public int GenderId { get; set; }
+        public Gender? Gender { get; set; }
+    }
+}
