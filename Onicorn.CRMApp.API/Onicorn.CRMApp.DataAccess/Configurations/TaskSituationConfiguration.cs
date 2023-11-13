894f3eceb21357c3aab16598d046d708d35b6e0e
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-13 18:00:52 +0300

    feat: TaskSituation entity için Configuration yapıldı.
    TaskSituation tablosu için veri tabanındaki kolonların özelliklerini etkileyecek birtakım ayarlar yapıldı.

---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.DataAccess/Configurations/TaskSituationConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Onicorn.CRMApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onicorn.CRMApp.DataAccess.Configurations
+{
+    public class TaskSituationConfiguration : IEntityTypeConfiguration<TaskSituation>
+    {
+        public void Configure(EntityTypeBuilder<TaskSituation> builder)
+        {
+            builder.HasData(new TaskSituation[]
+          {
+                new()
+                {
+                    Id = 1,
+                    Definition = "Completed"
+                },
+                new()
+                {
+                    Id = 2,
+                    Definition = "Continues"
+                },
+                 new()
+                {
+                    Id = 3,
+                    Definition = "Postponed"
+                },
+                 new()
+                {
+                    Id = 4,
+                    Definition = "Cancelled"
+                }
+          });
+            builder.Property(x => x.Definition).IsRequired().HasMaxLength(100);
+        }
+    }
+}
