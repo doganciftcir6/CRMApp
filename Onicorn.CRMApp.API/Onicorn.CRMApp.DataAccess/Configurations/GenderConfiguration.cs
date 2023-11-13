7bf394f918c5b9b4e6d00b23d78a2e6ad7af45ab
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-13 17:26:01 +0300

    feat: Gender entity için Configuration yapıldı.
    Gender tablosu için veri tabanındaki kolonların özelliklerini etkileyecek birtakım ayarlar yapıldı.

---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.DataAccess/Configurations/GenderConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Onicorn.CRMApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onicorn.CRMApp.DataAccess.Configurations
+{
+    public class GenderConfiguration : IEntityTypeConfiguration<Gender>
+    {
+        public void Configure(EntityTypeBuilder<Gender> builder)
+        {
+            builder.HasData(new Gender[]
+            {
+                new()
+                {
+                    Definition = "Male",
+                    Id = 1,
+                },
+                new()
+                {
+                    Definition = "Female",
+                    Id = 2,
+                },
+                 new()
+                {
+                    Definition = "I don't want to specify",
+                    Id = 3
+                }
+            });
+            builder.Property(x => x.Definition).IsRequired().HasMaxLength(100);
+        }
+    }
+}
