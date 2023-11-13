b2ba0b013cdffde1cc3f0d62e48b7dd8843e7c0b
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-13 16:00:25 +0300

    feat: Customer sınıfı oluşturuldu.
    Customerların yani iş yapacağımız şirketlerin bilgilerini tutacak olan tablomuzdan bu sınıf sorumlu olacak.

---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.Entities/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onicorn.CRMApp.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string? CompanyName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+        public string? Province { get; set; }
+        public string? District { get; set; }
+        public DateTime InsertTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public bool Status { get; set; }
+
+        public List<Communication>? Communications { get; set; }
+        public List<Sale>? Sales { get; set; }
+    }
+}
