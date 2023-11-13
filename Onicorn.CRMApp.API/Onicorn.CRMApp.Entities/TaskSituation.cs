9efd8ba6bcdd26d3509e81aee9bb773e6051887b
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-13 15:52:57 +0300

    feat: TaskSituation sınıfı oluşturuldu.
    Task Entity ile ilişkili olan TaskSituation entityin tanımlaması yapıldı.

---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.Entities/TaskSituation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onicorn.CRMApp.Entities
+{
+    public class TaskSituation
+    {
+        public int Id { get; set; }
+        public string? Definition { get; set; }
+        public List<Task>? Tasks { get; set; }
+    }
+}
