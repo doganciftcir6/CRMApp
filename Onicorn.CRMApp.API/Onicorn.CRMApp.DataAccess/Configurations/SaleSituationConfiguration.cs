a522d8370185cf2c093a88bb49809bd8ba685aaf
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-13 17:47:40 +0300

    feat: SaleSituation entity için Configuration yapıldı.
    SaleSituation tablosu için veri tabanındaki kolonların özelliklerini etkileyecek birtakım ayarlar yapıldı.

---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.DataAccess/Configurations/SaleSituationConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Onicorn.CRMApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onicorn.CRMApp.DataAccess.Configurations
+{
+    public class SaleSituationConfiguration : IEntityTypeConfiguration<SaleSituation>
+    {
+        public void Configure(EntityTypeBuilder<SaleSituation> builder)
+        {
+            builder.HasData(new SaleSituation[]
+           {
+                new()
+                {
+                    Id = 1,
+                    Definition = "Completed"
+                },
+                new()
+                {
+                    Id = 2,
+                    Definition = "Continues"
+                },
+                 new()
+                {
+                    Id = 3,
+                    Definition = "Cancelled"
+                }
+           });
+            builder.Property(x => x.Definition).IsRequired().HasMaxLength(100);
+        }
+    }
+}
