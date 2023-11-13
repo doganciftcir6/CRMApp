7e9d01902ae42a92ec52e4c5e082eebcfa9dc222
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-13 16:19:15 +0300

    feat: Sale sınıfı oluşturuldu.
    Şirketin tamamladığı projelerin müşterilere satış bilgileri bu tabloda tutulacak.

---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.Entities/Sale.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onicorn.CRMApp.Entities
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+        public int SalesAmount { get; set; }
+        public DateTime SalesDate { get; set; }
+        public bool Status { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+
+        public int ProjectId { get; set; }
+        public Project? Project { get; set; }
+    }
+}
