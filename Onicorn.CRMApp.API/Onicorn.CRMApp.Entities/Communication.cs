bb8915ac15b5f533b03bb4259b47945ec3acb356
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-13 16:08:25 +0300

    feat: Communication sınıfı oluşturuldu.
    Customerlar ile şirket arasında kurulacak olan iletişimler bu tabloda tutulacak.

---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.Entities/Communication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onicorn.CRMApp.Entities
+{
+    public class Communication
+    {
+        public int Id { get; set; }
+        public DateTime CommunicationDate { get; set; }
+        public string? Detail { get; set; }
+        public DateTime InsertTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public bool Status { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+
+        public int CommunicationTypeId { get; set; }
+        public CommunicationType? CommunicationType { get; set; }
+    }
+}
