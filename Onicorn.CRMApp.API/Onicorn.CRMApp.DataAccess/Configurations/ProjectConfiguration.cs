eed7fd7cd15257f9c2f7e218f2dfb8d4c1ea434f
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-13 17:32:08 +0300

    feat: Project entity için Configuration yapıldı.
    Project tablosu için veri tabanındaki kolonların özelliklerini etkileyecek birtakım ayarlar yapıldı. Ayrıca fiyat alanı decimal olarak değiştirildi.

---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.DataAccess/Configurations/ProjectConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Onicorn.CRMApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onicorn.CRMApp.DataAccess.Configurations
+{
+    public class ProjectConfiguration : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder.Property(x => x.ProjectName).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Description).IsRequired().HasMaxLength(500);
+            builder.Property(x => x.Price).IsRequired().HasMaxLength(20);
+            builder.Property(x => x.ImageURL).IsRequired();
+            builder.Property(x => x.InsertTime).HasDefaultValueSql("getdate()");
+        }
+    }
+}
--- Onicorn.CRMApp.API/Onicorn.CRMApp.Entities/Project.cs
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.Entities/Project.cs
@@ -13,7 +13,7 @@ namespace Onicorn.CRMApp.Entities
         public int Id { get; set; }
         public string? ProjectName { get; set; }
         public string? Description { get; set; }
-        public int Price { get; set; }
+        public decimal Price { get; set; }
         public string? ImageURL { get; set; }
         public DateTime InsertTime { get; set; }
         public DateTime UpdateTime { get; set; }
