abdb53d0f811e719e17ccd98ee404f300dc1acb8
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-13 17:22:28 +0300

    feat: Customer entity için Configuration yapıldı.
    Customer tablosu için veri tabanındaki kolonların özelliklerini etkileyecek birtakım ayarlar yapıldı.

---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.DataAccess/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Onicorn.CRMApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onicorn.CRMApp.DataAccess.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.Property(x => x.CompanyName).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Email).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Phone).IsRequired().HasMaxLength(11);
+            builder.Property(x => x.Address).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Province).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.District).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.InsertTime).HasDefaultValueSql("getdate()");
+        }
+    }
+}
