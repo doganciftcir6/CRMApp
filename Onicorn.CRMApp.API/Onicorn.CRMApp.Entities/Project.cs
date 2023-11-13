b1f24ce4731d1655a9aa3cb7def1aa3afe5753ad
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-13 16:27:53 +0300

    feat: Project sınıfı oluşturuldu.
    Şirketin geliştireceği projeler bu tabloda tutulacak.

---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.Entities/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onicorn.CRMApp.Entities
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string? ProjectName { get; set; }
+        public string? Description { get; set; }
+        public int Price { get; set; }
+        public string? ImageURL { get; set; }
+        public DateTime InsertTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public bool Status { get; set; }
+
+        public List<Sale>? Sales { get; set; }
+    }
+}
