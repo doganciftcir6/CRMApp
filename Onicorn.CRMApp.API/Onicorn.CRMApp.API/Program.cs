a917142f2f61539e7983d8d67c732e5a02127a91
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-14 15:46:10 +0300

    feat: DependencyExtension sınıfı oluşturuldu.
    DependencyExtension sınıfı oluşturularak program.cs tarafında bulunan Ioc Container'ı business katmanına genişletilmiş oldu. Bu sayede hem program.cs tarafı daha temiz kalacak hemde UI uygulaması değişirse bu ayarları o yeni UI uygulaması için tekrar tekrar yapmak zorunda değiliz. Ayarları altyapımıza yedirmiş olduk.

--- Onicorn.CRMApp.API/Onicorn.CRMApp.API/Program.cs
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Onicorn.CRMApp.Business.CustomDescriber;
+using Onicorn.CRMApp.Business.DependencyResolvers.Microsoft;
 using Onicorn.CRMApp.Business.Services.Concrete;
 using Onicorn.CRMApp.Business.Services.Interfaces;
 using Onicorn.CRMApp.DataAccess.Contexts.EntityFramework;
@@ -10,24 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-//Identity
-builder.Services.AddIdentity<AppUser, AppRole>(opt =>
-{
-    opt.Password.RequireDigit = true;
-    opt.Password.RequiredLength = 1;
-    opt.Password.RequireUppercase = true;
-    opt.Password.RequireLowercase = true;
-    opt.Password.RequireNonAlphanumeric = true;
-    opt.Lockout.MaxFailedAccessAttempts = 5;
-}).AddErrorDescriber<CustomErrorDescriber>().AddEntityFrameworkStores<AppDbContext>();
+//Ioc Extension Ýmplement
+builder.Services.AddDependencies(builder.Configuration);
 
-builder.Services.AddDbContext<AppDbContext>(opt =>
-{
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnection"));
-});
-
-builder.Services.AddScoped<IUow, Uow>();
-builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Onicorn.CRMApp.Business.CustomDescriber;
+using Onicorn.CRMApp.Business.Services.Concrete;
+using Onicorn.CRMApp.Business.Services.Interfaces;
+using Onicorn.CRMApp.DataAccess.Contexts.EntityFramework;
+using Onicorn.CRMApp.DataAccess.UnitOfWork;
+using Onicorn.CRMApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onicorn.CRMApp.Business.DependencyResolvers.Microsoft
+{
+    public static class DependencyExtension
+    {
+        public static void AddDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Identity
+            services.AddIdentity<AppUser, AppRole>(opt =>
+            {
+                opt.Password.RequireDigit = true;
+                opt.Password.RequiredLength = 1;
+                opt.Password.RequireUppercase = true;
+                opt.Password.RequireLowercase = true;
+                opt.Password.RequireNonAlphanumeric = true;
+                opt.Lockout.MaxFailedAccessAttempts = 5;
+            }).AddErrorDescriber<CustomErrorDescriber>().AddEntityFrameworkStores<AppDbContext>();
+
+            //Context
+            services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("SqlServerConnection"));
+            });
+
+            //Scopes,Singletons,Transients
+            services.AddScoped<IUow, Uow>();
+            services.AddScoped<IAuthService, AuthService>();
+        }
+    }
+}
