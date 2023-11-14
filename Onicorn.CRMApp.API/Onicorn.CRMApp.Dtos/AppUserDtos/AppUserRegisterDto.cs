8c766a9d094ad62eb2128975abdfe1ed92ae604f
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-14 13:56:36 +0300

    feat: AppUserRegisterDto classı tanımlandı.
    Register olma işlemi sırasında kullanıcılardan bilgileri alacak olan AppUserRegisterDto classı tanımlandı.

---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.Dtos/AppUserDtos/AppUserRegisterDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onicorn.CRMApp.Dtos.AppUserDtos
+{
+    public class AppUserRegisterDto
+    {
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? Email { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public string? ImageURL { get; set; }
+
+        public int GenderId { get; set; }
+    }
+}
