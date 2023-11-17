46edd56206b9a77f1b75b14f6af68a5d4efebb57
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-17 16:49:48 +0300

    feat: Asp.Net Core MVC frontend uygulaması oluşturuldu.
    Yazılan API'ı tüketecek olan frontend uygulamasının temel dosyaları oluşturuldu.

---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Onicorn.CRMApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
