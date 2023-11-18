860a623eac060e260fccfe2c6b255b4c50342e38
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-18 22:18:24 +0300

    feat: UpdateProfile işlemleri tamamlandı.
    UpdateProfile sayfası oluşturuldu ve put metotunda API ile haberleşmesi sağlanarak kullanıcının profil bilgilerini değiştirme özelliği kullanılabilir hale geldi.

--- Onicorn.CRMApp.API/Onicorn.CRMApp.Web/Controllers/AppUserController.cs
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.Web/Controllers/AppUserController.cs
@@ -24,7 +24,7 @@ namespace Onicorn.CRMApp.Web.Controllers
                 _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
                 var response = await _httpClient.GetAsync("AppUser/GetProfile");
                 if (response is null || response.StatusCode == HttpStatusCode.InternalServerError)
-                    ModelState.AddModelError("", "Authentication server error. Please try again later.");
+                    ModelState.AddModelError("", "Server error. Please try again later.");
 
 
                 CustomResponse<AppUserProfileVM> appUserProfileVM = await response.Content.ReadFromJsonAsync<CustomResponse<AppUserProfileVM>>();
@@ -38,6 +38,71 @@ namespace Onicorn.CRMApp.Web.Controllers
                 }
             }
             return View();
+        }
+
+        public async Task<IActionResult> UpdateProfile()
+        {
+            var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
+            if (token != null)
+            {
+                var _httpClient = _httpClientFactory.CreateClient("MyApiClient");
+                _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                var response = await _httpClient.GetAsync("AppUser/GetProfile");
+                if (response is null || response.StatusCode == HttpStatusCode.InternalServerError)
+                    ModelState.AddModelError("", "Server error. Please try again later.");
+
+
+                CustomResponse<AppUserUpdateProfileInput> appUserProfileVM = await response.Content.ReadFromJsonAsync<CustomResponse<AppUserUpdateProfileInput>>();
+                if (appUserProfileVM.IsSuccessful)
+                {
+                    return View(appUserProfileVM.Data);
+                }
+                foreach (var error in appUserProfileVM.Errors)
+                {
+                    ModelState.AddModelError("", error);
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateProfile(AppUserUpdateProfileInput appUserUpdateProfileInput)
+        {
+            if (ModelState.IsValid)
+            {
+                var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
+                if (token != null)
+                {
+                    var _httpClient = _httpClientFactory.CreateClient("MyApiClient");
+                    _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                    var multipartContent = new MultipartFormDataContent();
+                    if (appUserUpdateProfileInput.ImageFile is not null)
+                    {
+                        using var ms = new MemoryStream();
+                        ms.Position = 0;
+                        await appUserUpdateProfileInput.ImageFile.CopyToAsync(ms);
+                        multipartContent.Add(new ByteArrayContent(ms.ToArray()), "ImageURL", appUserUpdateProfileInput.ImageFile.FileName);
+                    }
+                    multipartContent.Add(new StringContent(appUserUpdateProfileInput.Firstname), "Firstname");
+                    multipartContent.Add(new StringContent(appUserUpdateProfileInput.Lastname), "Lastname");
+                    multipartContent.Add(new StringContent(appUserUpdateProfileInput.Email), "Email");
+                    multipartContent.Add(new StringContent(appUserUpdateProfileInput.PhoneNumber.ToString()), "PhoneNumber");
+
+                    var response = await _httpClient.PutAsync("AppUser/UpdateProfile", multipartContent);
+                    if (response is null || response.StatusCode == HttpStatusCode.InternalServerError)
+                        ModelState.AddModelError("", "Server error. Please try again later.");
+
+                    CustomResponse<NoContent> updateProfileResponse = await response.Content.ReadFromJsonAsync<CustomResponse<NoContent>>();
+                    if (updateProfileResponse.IsSuccessful)
+                    {
+                        return RedirectToAction(nameof(ProfileDetails));
+                    }
+                    foreach (var error in updateProfileResponse.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+            }
+            return View(appUserUpdateProfileInput);
         }
     }
 }
---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.Web/Models/AppUserUpdateProfileInput.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Onicorn.CRMApp.Web.Models
+{
+    public class AppUserUpdateProfileInput
+    {
+        [Required(ErrorMessage = "The firstname field is required.")]
+        public string? Firstname { get; set; }
+        [Required(ErrorMessage = "The lastname field is required.")]
+        public string? Lastname { get; set; }
+        [Required(ErrorMessage = "The phone number field is required.")]
+        public string? PhoneNumber { get; set; }
+        [Required(ErrorMessage = "The email field is required.")]
+        public string? Email { get; set; }
+        public string? ImageURL { get; set; }
+        public IFormFile? ImageFile { get; set; }
+    }
+}
