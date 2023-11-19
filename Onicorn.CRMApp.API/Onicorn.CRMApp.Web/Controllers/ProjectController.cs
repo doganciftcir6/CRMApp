f6118a366b8803708eb0c7f17eb17f19f6d5a677
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-19 21:16:35 +0300

    feat: Project ana sayfası oluşturuldu.
    Tüm projectlerin listelendiği bir ana sayfa oluşturuldu. Bunun için API ile haberleşme sağlandı.

---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.Web/Controllers/ProjectController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Onicorn.CRMApp.Shared.Utilities.Response;
+using Onicorn.CRMApp.Web.Models;
+
+namespace Onicorn.CRMApp.Web.Controllers
+{
+    [Authorize]
+    public class ProjectController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ProjectController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
+            if (token != null)
+            {
+                var _httpClient = _httpClientFactory.CreateClient("MyApiClient");
+                _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                var response = await _httpClient.GetAsync("Project/GetProjects");
+                if (response.IsSuccessStatusCode)
+                {
+                    CustomResponse<IEnumerable<ProjectsVM>> projectsResponse = await response.Content.ReadFromJsonAsync<CustomResponse<IEnumerable<ProjectsVM>>>();
+                    return View(projectsResponse.Data);
+                }
+            }
+            return View();
+        }
+    }
+}
