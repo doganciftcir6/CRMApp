3e410e1ea0fda1b03d6b50f77acde34f0df350e3
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-19 19:55:40 +0300

    feat: Satış anasayfası oluşturuldu.
    API ile haberleşip satışların listelendiği sayfa oluşturuldu.

---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.Web/Controllers/SaleController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Onicorn.CRMApp.Shared.Utilities.Response;
+using Onicorn.CRMApp.Web.Models;
+
+namespace Onicorn.CRMApp.Web.Controllers
+{
+    public class SaleController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public SaleController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
+            if (token != null)
+            {
+                var _httpClient = _httpClientFactory.CreateClient("MyApiClient");
+                _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                var response = await _httpClient.GetAsync("Sale/GetSales");
+                if (response.IsSuccessStatusCode)
+                {
+                    CustomResponse<IEnumerable<SalesVM>> salesResponse = await response.Content.ReadFromJsonAsync<CustomResponse<IEnumerable<SalesVM>>>();
+                    return View(salesResponse.Data);
+                }
+            }
+            return View();
+        }
+
+    }
+}
---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.Web/Models/SalesVM.cs
@@ -0,0 +1,17 @@
+﻿namespace Onicorn.CRMApp.Web.Models
+{
+    public class SalesVM
+    {
+        public int Id { get; set; }
+        public decimal SalesAmount { get; set; }
+        public int KDV { get; set; } 
+        public decimal TotalSalesAmount { get; set; } 
+        public DateTime SalesDate { get; set; }
+        public bool Status { get; set; }
+
+
+        public string? Customer { get; set; }
+        public string? Project { get; set; }
+        public string? SaleSituation { get; set; }
+    }
+}
