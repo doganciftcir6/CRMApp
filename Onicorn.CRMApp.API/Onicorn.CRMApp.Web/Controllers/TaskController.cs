912936610f3ff4ef922eee3b6bbb8febc9e77a69
Dogan Ciftci <doganciftcir6@hotmail.com>
2023-11-20 13:39:23 +0300

    feat: Task listeleme sayfaları oluşturuldu.
    Tüm taskların listelendiği ve sadece kullanıcının kendi tasklarının listelendiği 2 adet sayfa oluşturulup API ile haberleşmeleri sağlandı.

---
+++ Onicorn.CRMApp.API/Onicorn.CRMApp.Web/Controllers/TaskController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Onicorn.CRMApp.Shared.Utilities.Response;
+using Onicorn.CRMApp.Web.Models;
+
+namespace Onicorn.CRMApp.Web.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public TaskController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
+            if (token != null)
+            {
+                var _httpClient = _httpClientFactory.CreateClient("MyApiClient");
+                _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                var response = await _httpClient.GetAsync("Task/GetTasks");
+                if (response.IsSuccessStatusCode)
+                {
+                    CustomResponse<IEnumerable<TasksVM>> tasksResponse = await response.Content.ReadFromJsonAsync<CustomResponse<IEnumerable<TasksVM>>>();
+                    return View(tasksResponse.Data);
+                }
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> MyTasks()
+        {
+            var token = User.Claims.FirstOrDefault(x => x.Type == "accessToken")?.Value;
+            if (token != null)
+            {
+                var _httpClient = _httpClientFactory.CreateClient("MyApiClient");
+                _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                var response = await _httpClient.GetAsync("Task/GetTasksByUser");
+                if (response.IsSuccessStatusCode)
+                {
+                    CustomResponse<IEnumerable<TasksVM>> tasksResponse = await response.Content.ReadFromJsonAsync<CustomResponse<IEnumerable<TasksVM>>>();
+                    return View(tasksResponse.Data);
+                }
+            }
+            return View();
+        }
+    }
+}
